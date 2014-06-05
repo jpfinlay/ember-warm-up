17ebfe6c63c81478f803c6a7c21c6ed3c064dda7
Jamie Finlay <jpfinlay@gmail.com>
2014-06-05 15:29:35 +0100

    Added index and app.js this time

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
   this.route('about');
+  this.resource('products');
 });
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
@@ -18,4 +19,27 @@ App.AboutController = Ember.Controller.extend({
   open: function() {
     return ((new Date()).getDay() === 0) ? "Sorry, Closed" : "We're Open";
   }.property()
+});
+
+App.PRODUCTS = [
+{
+  title: 'Flint',
+  price: 9.99,
+  description: 'Nice flint',
+  isOnSale: true,
+  image: ('images/products/flint.png')
+},
+{
+  title: 'Kindling',
+  price: 9.99,
+  description: 'Nice kindling',
+  isOnSale: true,
+  image: ('images/products/kindling.png')
+}
+];
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return App.PRODUCTS;
+  }
 });
