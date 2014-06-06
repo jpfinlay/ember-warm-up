59ff7119c613410eafcfbd3fdf8933908f9d7510
Jamie Finlay <jpfinlay@gmail.com>
2014-06-06 15:52:27 +0100

    Merged about and contacts pages and nested product into products page. Completed Level 3

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ var App = Ember.Application.create({
 });
 App.Router.map(function() {
   this.route('credits', { path: '/thanks' });
-  this.route('about');
-  this.resource('products');
+  this.resource('products', function() {
+    this.resource('product', { path: '/:title' });
+  });
+  this.resource('contacts', function() { 
+    this.resource('contact', { path: '/:name' });
+  });
 });
+
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
   logo: 'images/logo-small.png',
@@ -13,33 +18,109 @@ App.IndexController = Ember.Controller.extend({
     return (new Date()).toDateString();
   }.property()
 });
-App.AboutController = Ember.Controller.extend({
-  contactName: 'Jamie',
+App.ContactsIndexController = Ember.Controller.extend({
+  contactName: 'Anostagia',
   avatar: 'images/avatar.png',
   open: function() {
-    return ((new Date()).getDay() === 0) ? "Sorry, Closed" : "We're Open";
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
   }.property()
 });
-
-App.PRODUCTS = [
-{
-  title: 'Flint',
-  price: 9.99,
-  description: 'Nice flint',
-  isOnSale: true,
-  image: ('images/products/flint.png')
-},
-{
-  title: 'Kindling',
-  price: 9.99,
-  description: 'Nice kindling',
-  isOnSale: true,
-  image: ('images/products/kindling.png')
-}
-];
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
     return App.PRODUCTS;
   }
 });
+App.ProductRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.PRODUCTS.findBy('title', params.title); 
+  }
+});
+
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return App.CONTACTS;
+  }
+});
+App.ContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.CONTACTS.findBy('name', params.name);
+  }
+});
+
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+    isOnSale: true,
+    image: 'images/products/flint.png'
+  },
+  {
+    title: 'Kindling',
+    price: 249,
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
+    isOnSale: false,
+    image: 'images/products/kindling.png'
+  }
+];
+
+App.CONTACTS = [
+  {
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'images/contacts/giamia.png'
+  },
+  {
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'images/contacts/anostagia.png'
+  }
+];
+
+
+//var App = Ember.Application.create({
+//   LOG_TRANSITIONS: true
+// });
+// App.Router.map(function() {
+//   this.route('credits', { path: '/thanks' });
+//   this.route('about');
+//   this.resource('products');
+// });
+// App.IndexController = Ember.Controller.extend({
+//   productsCount: 6,
+//   logo: 'images/logo-small.png',
+//   time: function() {
+//     return (new Date()).toDateString();
+//   }.property()
+// });
+// App.AboutController = Ember.Controller.extend({
+//   contactName: 'Jamie',
+//   avatar: 'images/avatar.png',
+//   open: function() {
+//     return ((new Date()).getDay() === 0) ? "Sorry, Closed" : "We're Open";
+//   }.property()
+// });
+
+// App.PRODUCTS = [
+// {
+//   title: 'Flint',
+//   price: 9.99,
+//   description: 'Nice flint',
+//   isOnSale: true,
+//   image: ('images/products/flint.png')
+// },
+// {
+//   title: 'Kindling',
+//   price: 9.99,
+//   description: 'Nice kindling',
+//   isOnSale: true,
+//   image: ('images/products/kindling.png')
+// }
+// ];
+
+// App.ProductsRoute = Ember.Route.extend({
+//   model: function() {
+//     return App.PRODUCTS;
+//   }
+// });
